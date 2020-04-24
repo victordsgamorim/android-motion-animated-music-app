3110bea0b4cb0f137b4e260513fd80414bc82d7b
Victor Amorim <victordsgamorim@gmail.com>
2020-04-24 22:52:35 +0100

    animated xml refactor

--- app/src/main/java/com/victor/musicapp/MainActivity.kt
+++ app/src/main/java/com/victor/musicapp/MainActivity.kt
@@ -1,56 +1,44 @@
 package com.victor.musicapp
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import com.victor.musicapp.util.animationLoop
 import com.victor.musicapp.util.setAnimationToDrawable
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.end_motion_extended_card_info.*
-import kotlinx.android.synthetic.main.scrolling_bottom_details.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private var isClicked = true
 
-    private val topLeftForward by lazy {
-        AnimatedVectorDrawableCompat.create(this, R.drawable.wave_forward)
+    private val cardWaveForward by lazy{
+        AnimatedVectorDrawableCompat.create(this, R.drawable.animated_card_wave_forward)
     }
 
-    private val topLeftReverse by lazy {
-        AnimatedVectorDrawableCompat.create(this, R.drawable.wave_reverse)
+    private val cardWaveBackward by lazy{
+        AnimatedVectorDrawableCompat.create(this, R.drawable.animated_card_wave_backward)
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fluidTopLeft.setAnimationToDrawable(topLeftForward)
+        setWaveAnimationToCard()
+        listenFloatActionButton()
 
+    }
+
+    private fun setWaveAnimationToCard() {
+        album_card_wave_animation.setAnimationToDrawable(cardWaveForward)
+        album_card_wave_animation.animationLoop(cardWaveForward, cardWaveBackward)
+    }
+
+    private fun listenFloatActionButton() {
         main_listen_button.setOnClickListener {
             toggleIconState()
         }
-
-
-        /**callbacks*/
-        topLeftForward?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
-            override fun onAnimationEnd(drawable: Drawable?) {
-                fluidTopLeft.setImageDrawable(topLeftReverse)
-                topLeftReverse?.start()
-            }
-        })
-
-        topLeftReverse?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
-            override fun onAnimationEnd(drawable: Drawable?) {
-                fluidTopLeft.setImageDrawable(topLeftForward)
-                topLeftForward?.start()
-            }
-        })
-
-        topLeftForward?.start()
     }
 
 
